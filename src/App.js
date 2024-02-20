81d1fa71e83ed76cfe18c9149715467d8317e9b0
mohamed <moa.mahfouz@gmail.com>
2024-02-20 23:50:53 +0200

    adding first styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
-
+import './style.css'
 const App = () => (
     <div>
-        <h1>React App From Scratch!</h1>
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Privacy</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </nav>
     </div>
 );
 
